8d974ffe4459e08973101cba161c48a485a5424d
martincollegeonline <mgleeson.studygroup@gmail.com>
2014-05-03 16:21:51 +1000

    initial commit

---
+++ 1428/s34_a1_01.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CDemoLib
+{
+    public class Customer
+    {
+        private int _age;
+        private string _name;
+
+        public Customer()
+        {
+            Age = 0;
+            Name = "Not available";
+        }
+
+        public int Age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+    }
+}
---
+++ 1428/s34_a1_02.cs
@@ -0,0 +1,5 @@
+Customer myCustomer = new Customer();
+myCustomer.Name = "Kevin Browne";
+myCustomer.Age = 42;
+Label1.Text = "Name: " + myCustomer.Name +
+    "<br/>Age: " + myCustomer.Age.ToString();
